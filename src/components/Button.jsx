8d832fffc32ff9e75bf9afb9a49dc8aac6d461af
Rohi Ulecia <rohi.ulecia@gmail.com>
2021-01-03 10:32:09 +0200

    fixed button cursor and updated snapshots

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,6 @@ import React from "react";
 export default function ButtonLink({ value, onClick }) {
     
     return (
-        <input type="button" value={value} onClick={onClick} className="w-1/2 rounded-lg text-center text-white bg-blue-500 hover:bg-blue-600 border-black border-2" />
+        <input type="button" value={value} onClick={onClick} className="w-1/2 rounded-lg text-center text-white bg-blue-500 hover:bg-blue-600 cursor-pointer border-black border-2" />
     );
 }
